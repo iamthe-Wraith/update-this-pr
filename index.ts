d49360450b23499dbef3618f9ed2ed56a2477c32
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-09-13 19:05:15 -0400

    adds support for regex from branch matching

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ core.getInput('myInput');
 const token = core.getInput('token', { required: true });
 const top = core.getInput('top');
 const bottom = core.getInput('bottom');
+const topFromBranch = core.getInput('top-from-branch');
+const bottomFromBranch = core.getInput('bottom-from-branch');
 
 const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
 
@@ -25,11 +27,21 @@ const { data: pullRequest } = await octokit.rest.pulls.get({
 
 let body = '';
 
-if (top) body += `${top}\n`;
+if (top) body += `${top}\n\n`;
+if (topFromBranch && !top) {
+  const branch = pullRequest.head.ref;
+  const match = new RegExp(topFromBranch, 'gmi').exec(branch);
+  if (match?.length) body += `${match[0].toUpperCase()}\n\n`;
+}
 
 body += pullRequest.body;
 
-if (bottom) body += `\n${bottom}`;
+if (bottom) body += `\n\n${bottom}`;
+if (bottomFromBranch && !bottom) {
+  const branch = pullRequest.head.ref;
+  const match = new RegExp(bottomFromBranch, 'gmi').exec(branch);
+  if (match?.length) body += `${match[0].toUpperCase()}\n\n`;
+}
 
 octokit.rest.pulls.update({
   owner: repoOwner,
