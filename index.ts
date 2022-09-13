77f64342dc098fddde40af5954197404d652ae97
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-09-13 18:22:40 -0400

    updates pull request data

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
 import * as core from '@actions/core';
 import github from '@actions/github';
 
+core.getInput('myInput');
+
 const token = core.getInput('token', { required: true });
 // const top = core.getInput('top');
 // const bottom = core.getInput('bottom');
 
-// const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
+const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
 
-// const prNum = github.context.payload.pull_request.number;
+const prNum = github.context.payload.pull_request.number;
 
 const octokit = github.getOctokit(token);
 
 
 const { data: pullRequest } = await octokit.rest.pulls.get({
-  owner: 'octokit',
-  repo: 'rest.js',
-  pull_number: 123,
+  owner: repoOwner,
+  repo: repoName,
+  pull_number: prNum,
   mediaType: {
     format: 'diff'
   }
